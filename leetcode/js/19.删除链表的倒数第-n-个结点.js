8d1d4e56afcaed357c3eb5f8f975d5b24cc7bf4c
sz.ma <mazhoushan@didiglobal.com>
2021-10-22 14:53:38 +0800

    feat: 刷题

--- leetcode/js/19.删除链表的倒数第-n-个结点.js
+++ leetcode/js/19.删除链表的倒数第-n-个结点.js
@@ -17,8 +17,26 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
+  // 前后指针
+  let dummy = new ListNode(0, head)
+  let first = head
+  let second = dummy
 
+  for (let i = 0; i < n; ++i) {
+    first = first.next;
+  }
+
+  while (first) {
+    second = second.next
+    first = first.next
+  }
+
+  second.next = second.next.next
+
+  let node = dummy.next
+  return node
 };
 // @lc code=end
 
+removeNthFromEnd([1, 2, 3, 4, 5], 2)
